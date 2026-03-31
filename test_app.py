d071b2ba486f5b51bc6d5a040bac0939468852d1
mahekshahbad <129206584+mahekshahbad@users.noreply.github.com>
2026-03-31 16:00:06 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,3 +5,22 @@ def test_add():
 
 AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
 AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
+
+
+name: Trigger TruffleHog Scan
+
+on:
+  push:
+  pull_request:
+
+jobs:
+  call-trufflehog:
+    uses: medica-dev-platform/test-access/.github/workflows/trufflehog.yml@main
+
+    with:
+      working-directory: .
+      exclude-patterns: |
+        *.md
+        docs/
+      fail-on-secrets: true
